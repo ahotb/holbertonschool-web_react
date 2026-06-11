718936c37e69f3ddd73e35ae6187e5d2f0c1904d
ahotb <ah.r9625@gmail.com>
2026-06-12 00:17:56 +0300

    Add initial TypeScript setup with ESLint, Webpack, and sample student data

---
+++ task_0/js/main.ts
@@ -0,0 +1,39 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: 'abdullah',
+  lastName: 'manahi',
+  age: 28,
+  location: 'AL Khobar',
+};
+
+const student2: Student = {
+  firstName: 'manahi',
+  lastName: 'almouribd',
+  age: 22,
+  location: 'AL Khobar',
+};
+
+const studentsList: Student[] = [student1, student2];
+const table = document.createElement('table');
+document.body.appendChild(table);
+
+studentsList.forEach((student: Student) => {
+  const row = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+  nameCell.textContent = student.firstName;
+
+  const locationCell = document.createElement('td');
+  locationCell.textContent = student.location;
+
+  row.appendChild(nameCell);
+  row.appendChild(locationCell);
+
+  table.appendChild(row);
+});
