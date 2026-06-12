bf98a0136571a9e520801af991b300286f6f353e
ahotb <ah.r9625@gmail.com>
2026-06-12 16:52:16 +0300

    Add printTeacher function to format teacher names

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -29,3 +29,15 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: PrintTeacherFunction = (
+  firstName: string,
+  lastName: string,
+): string => {
+  return `${firstName[0]}. ${lastName}`;
+};
+console.log(printTeacher('John', 'Doe'));
