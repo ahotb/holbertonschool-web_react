29256962ec6e29429e4462a74a332550cde1370d
ahotb <ah.r9625@gmail.com>
2026-06-12 17:14:41 +0300

    Add Teacher interface, extend with Directors, and implement Student class
    - Created a Teacher interface with firstName and lastName properties.
    - Extended the Teacher interface to create a Directors interface with numberOfReports.
    - Added a PrintTeacherFunction interface for printing teacher names.
    - Implemented a StudentClass that adheres to the StudentInterface with methods for homework and displaying names.

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -1,3 +1,4 @@
+// interface -- 1. Let's build a Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -16,7 +17,7 @@ const teacher3: Teacher = {
 };
 
 console.log(teacher3);
-
+// extend -- 2. Extending the Teacher class
 interface Directors extends Teacher {
   numberOfReports: number;
 }
@@ -29,7 +30,7 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
-
+// Function -- 3. Printing teachers
 interface PrintTeacherFunction {
   (firstName: string, lastName: string): string;
 }
@@ -41,3 +42,31 @@ const printTeacher: PrintTeacherFunction = (
   return `${firstName[0]}. ${lastName}`;
 };
 console.log(printTeacher('John', 'Doe'));
+
+// Class -- 4. Writing a class
+interface StudentInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+class StudentClass implements StudentInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+  displayName(): string {
+    return this.firstName;
+  }
+}
