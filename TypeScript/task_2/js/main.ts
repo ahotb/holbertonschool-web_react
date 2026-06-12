caaf951b9886975e5d5a5ac8d49043e0b0692cd5
ahotb <ah.r9625@gmail.com>
2026-06-12 18:00:00 +0300

    fixed

--- TypeScript/task_2/js/main.ts
+++ TypeScript/task_2/js/main.ts
@@ -49,8 +49,7 @@ const createEmployee: createEmployee = (
 };
 
 console.log(createEmployee(200));
-Teacher;
+
 console.log(createEmployee(1000));
-Director;
+
 console.log(createEmployee('$500'));
-Director;
